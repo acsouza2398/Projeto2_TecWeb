57fcba69e31ae3cdebaa2ae8b1536755b58b6d33
Tiago Seixas <tvseixas@gmail.com>
2021-10-28 16:56:04 -0300

    adicionando chave do Tiago para a API

--- exemplo.py
+++ exemplo.py
@@ -8,7 +8,8 @@ querystring = {"r":"json","i":"tt4154796"}
 
 headers = {
     'x-rapidapi-host': "movie-database-imdb-alternative.p.rapidapi.com",
-    'x-rapidapi-key': "890d8b80cbmsh3b3238894fe0e6bp1069f8jsndeb50fd98ced"
+    #'x-rapidapi-key': "890d8b80cbmsh3b3238894fe0e6bp1069f8jsndeb50fd98ced" #chave Ana
+    #'x-rapidapi-key': '7c06e8aa11msh18a56dfb6b8a592p18b9e7jsn1c16abd3528f' #chave Tiago
     }
 
 response = requests.request("GET", url, headers=headers, params=querystring)
