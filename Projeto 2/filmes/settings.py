a0bb956bcf7ca320b7d7dbb3237c8c7718582818
acsouza2398 <63055644+acsouza2398@users.noreply.github.com>
2021-11-09 08:37:14 -0300

    Reconfigurando para sqlite

--- Projeto 2/filmes/settings.py
+++ Projeto 2/filmes/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'filmes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'films',
-        'USER': 'filmsuser',
-        'PASSWORD': 'filmssenha',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
