abeaddc4b06a417069c3a3f6f37bb1a688de6845
acsouza2398 <63055644+acsouza2398@users.noreply.github.com>
2021-11-14 03:40:03 -0300

    Preparando para o deploy 2

--- Projeto 1 - Parte B (base)/README.md
+++
@@ -1,5 +0,0 @@
-# Projeto 1 - Parte B
-
-Ana Carolina Souza - TechWeb 2021.2
-
-Link do site: https://stark-mesa-20771.herokuapp.com/
--- Projeto 1 - Parte B (base)/notes/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Note, Tag
-
-
-admin.site.register(Note)
-admin.site.register(Tag)
